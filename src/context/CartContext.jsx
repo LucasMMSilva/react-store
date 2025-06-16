48a5d556cac7594ba6947b5635d476c6e7bdf2a0
Lucas Manoel <dev.lucasmanoel@gmail.com>
2025-06-16 11:49:59 -0300

    feat: Implementation of localstorage in cartContext

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,31 +1,46 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-    const [cartItems, setCartItems] = useState([]);
+    const [cartItems, setCartItems] = useState(() => {
+        const storedCart = localStorage.getItem("cart");
+        return storedCart ? JSON.parse(storedCart) : [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem("cart", JSON.stringify(cartItems));
+    }, [cartItems]);
 
     const addToCart = (product) => {
         const existingItem = cartItems.find(item => item.id === product.id);
 
         if (existingItem) {
-            setCartItems(cartItems.map( item => item.id === product.id ? {...item, quantity: item.quantity + 1 } : item ));
+            setCartItems(cartItems.map(item =>
+                item.id === product.id
+                    ? { ...item, quantity: item.quantity + 1 }
+                    : item
+            ));
         } else {
             setCartItems([...cartItems, { ...product, quantity: 1 }]);
         }
-    }
+    };
 
     const removeFromCart = (id) => {
         setCartItems(cartItems.filter(item => item.id !== id));
-    }
+    };
 
     const updateQuantity = (id, quantity) => {
         if (quantity <= 0) {
             removeFromCart(id);
         } else {
-            setCartItems(cartItems.map(item => item.id === id ? {...item, quantity} : item ));
+            setCartItems(cartItems.map(item =>
+                item.id === id
+                    ? { ...item, quantity }
+                    : item
+            ));
         }
-    }
+    };
 
     const clearCart = () => {
         setCartItems([]);
@@ -43,7 +58,7 @@ export const CartProvider = ({ children }) => {
         >
             {children}
         </CartContext.Provider>
-    )
-}
+    );
+};
 
 export const useCart = () => useContext(CartContext);
