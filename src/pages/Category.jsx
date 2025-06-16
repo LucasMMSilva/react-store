bc77b8ca183f7bea7a90d6f4abb4c911c6bc9eae
Lucas Manoel <dev.lucasmanoel@gmail.com>
2025-06-16 10:18:33 -0300

    fix: Category Page

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,17 +1,17 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getProductsByCategory } from '../services/productService';
-import ProductCard from '../components/ProductCard';
+import ProductList from '../components/ProductList';
 
 function Category() {
-  const { categoryName } = useParams();
+  const { category } = useParams();
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
-
+  
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const data = await getProductsByCategory(categoryName);
+        const data = await getProductsByCategory(category);
         setProducts(data);
       } catch (error) {
         console.error('Erro ao buscar produtos da categoria:', error);
@@ -21,24 +21,20 @@ function Category() {
     };
 
     fetchProducts();
-  }, [categoryName]);
+  }, [category]);
 
   if (loading) {
-    return <p>Carregando...</p>;
+    return <p className='container'>Carregando...</p>;
   }
 
   if (!products.length) {
-    return <p>Nenhum produto encontrado nesta categoria.</p>;
+    return <p className='container'>Nenhum produto encontrado nesta categoria.</p>;
   }
 
   return (
-    <div>
-      <h2>Categoria: {categoryName}</h2>
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
-        {products.map(product => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
+    <div className='container'>
+      <h2>Categoria: {category}</h2>
+      <ProductList category={category} />
     </div>
   );
 }
